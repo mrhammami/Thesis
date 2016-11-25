cee0a8ffc9d4e424a52801e1ad69e6a427ee961e
mrhammami <michael.hammami@gmail.com>
2016-11-25 14:19:08 +0100

    First commit

---
+++ Thesis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Thesis.Startup))]
+namespace Thesis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
