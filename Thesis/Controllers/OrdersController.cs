7b835ee7b49786482fe06ef5ad091aac3c4ae089
mrhammami <michael.hammami@gmail.com>
2016-11-27 20:10:31 +0100

    Google maps and minor updates

---
+++ Thesis/Controllers/OrdersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Thesis.Models;
+
+namespace Thesis.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.DailyMenuHead).Include(o => o.Dish).Include(o => o.User);
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.MenuHeadID = new SelectList(db.DailyMenuHeads, "ID", "ID");
+            ViewBag.DishID = new SelectList(db.Dishes, "ID", "Name");
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserID,DishID,MenuHeadID")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MenuHeadID = new SelectList(db.DailyMenuHeads, "ID", "ID", order.MenuHeadID);
+            ViewBag.DishID = new SelectList(db.Dishes, "ID", "Name", order.DishID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", order.UserID);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MenuHeadID = new SelectList(db.DailyMenuHeads, "ID", "ID", order.MenuHeadID);
+            ViewBag.DishID = new SelectList(db.Dishes, "ID", "Name", order.DishID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", order.UserID);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserID,DishID,MenuHeadID")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MenuHeadID = new SelectList(db.DailyMenuHeads, "ID", "ID", order.MenuHeadID);
+            ViewBag.DishID = new SelectList(db.Dishes, "ID", "Name", order.DishID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", order.UserID);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
